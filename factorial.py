84d691ca80837a9a7ce171705a6c1a8ce983a33a
uma <umarani.pamu@outlook.com>
2024-06-18 23:04:29 +0530

    code is written

--- factorial.py
+++ factorial.py
@@ -7,3 +7,19 @@ example: for 3 as input;
 print result for 3*2*1*0! as 6
 """
 
+def find_factorial(number):
+    if number == 0 or number == 1:
+        return 1
+
+    return number * find_factorial(number - 1)
+    
+try:
+    user_input=input("enter a number:")
+    number=int(user_input)
+    if number < 0:
+        print("enter valid number starts with 0!")
+    else:
+        factorial=find_factorial(number)
+        print(factorial)
+except ValueError:    
+    print("Invalid input!!")
